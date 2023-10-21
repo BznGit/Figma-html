3c2588bcf62e2e8ce6d4827c29a31649556f81be
BznGit <bez-al@mail.ru>
2023-10-22 02:21:13 +0300

    rfr

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 window.onscroll = () => {
-    console.log('eded')
+
     const header = document.querySelector(".header");
-    console.log(header)
+
     const Y = window.scrollY;
     if (Y > 100) {
         header.classList.add('ops');
     } else if  (Y < 35) {
         header.classList.remove('ops');
     }
+    console.log(document.body.scrollHeight, Y)
+ if (Y > document.body.scrollHeight-900){
+    header.classList.remove('ops');
+ } 
 }
